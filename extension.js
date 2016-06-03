92af3e5f3eb04f2377532673994962c59ce12686
SukyTD <alin@alltrapdacic.com>
2016-06-04 02:08:02 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -101,7 +101,7 @@
       ],
       afkpositionCheck: 15,
       afkRankCheck: "ambassador",
-      motdEnabled: true,
+      motdEnabled: false,
       motdInterval: 5,
       motd: "We deliver that phat bass",
       filterChat: true,
