ec60da5fb6fe0a5d58ffee90e63434265fe23b6c
SukyTD <alin@alltrapdacic.com>
2016-06-30 21:05:34 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -25,7 +25,7 @@
         }
         bot.commands.bbcCommand = {
             command: 'bbc',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
+            rank: 'manager', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
